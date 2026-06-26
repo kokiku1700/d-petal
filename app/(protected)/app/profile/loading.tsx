3e1ff83684a451702e7bd18ce8d2cbaab456e295
kokiku1700 <kokiku1700@naver.com>
2026-06-26 16:51:57 +0900

    feat: 내 정보 페이지 로딩 ui 추가

--- app/(protected)/app/profile/loading.tsx
+++ app/(protected)/app/profile/loading.tsx
@@ -1,7 +1,17 @@
 import Spinner from "@/components/Spinner"
 
 const Loading = () => {
-    return <Spinner />
+    return (
+        <div 
+            className="
+                w-full h-screen 
+                flex flex-col justify-center items-center">
+            <Spinner />
+            <p className="text-sm italic">
+                꽃잎 정보를 불러오는 중...
+            </p>
+        </div>
+    )   
 };
 
 export default Loading;
